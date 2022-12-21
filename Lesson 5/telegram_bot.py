a0efd804a57997c578cf17a46f6d4fe362cf2ac7
Vladimir Danilov <voffka.danilov@gmail.com>
2022-12-21 17:05:59 +0300

    Update telegram_bot.py

--- Lesson 5/telegram_bot.py
+++ Lesson 5/telegram_bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
 
-app = ApplicationBuilder().token("5933564655:AAFztH1QvgGhVX5MEcmBeuCL8l3SIyd9o1w").build()
+app = ApplicationBuilder().token("your_token_here").build()
 
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("help", help_command))
